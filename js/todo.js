172b93567f3c45df55445143c3803a2d4c17eb96
javidalakbarli <73642700+javidalakbarli@users.noreply.github.com>
2022-11-15 11:49:41 +0400

    added cross out button

--- js/todo.js
+++ js/todo.js
@@ -4,24 +4,37 @@ $(document).ready(function () {
 
         let h3 = document.createElement("h3");
         let deleleteButton = document.createElement("button");
+        let crossoutButton = document.createElement("button");
         let listGrouping = document.createElement("div");
+        let buttonGrouping = document.createElement("div");
+
         h3.setAttribute("class", `all-texts h3-${i}`);
-        deleleteButton.setAttribute("class", `all-buttons delete-${i}`);
-        deleleteButton.setAttribute("onclick", `removeUl(${i})`);
-        listGrouping.setAttribute("class", `list-grouping-${i}`);
+        deleleteButton.setAttribute("class", `all-buttons all-delete-buttons delete-${i}`);
+        crossoutButton.setAttribute("class", `all-buttons all-crossout-buttons crossout-${i}`);
+        deleleteButton.setAttribute("onclick", `removeUlElement(${i})`);
+        crossoutButton.setAttribute("onclick", `crossUlElement(${i})`);
+        listGrouping.setAttribute("class", `list-groupings list-grouping-${i}`);
+        buttonGrouping.setAttribute("class", `button-groupings button-grouping-${i}`);
 
         i++;
 
         h3.innerText = $(".start-text").val();
         deleleteButton.innerText = "Delete";
+        crossoutButton.innerText = "Cross";
 
-        listGrouping.append(h3, deleleteButton);
+        buttonGrouping.append(deleleteButton, crossoutButton);
+        listGrouping.append(h3, buttonGrouping);
         $(".list").append(listGrouping);
 
         $(".start-text").val("");
     })
 })
 
-function removeUl(number) {
+function removeUlElement(number) {
     $(`.list-grouping-${number}`).remove();
+}
+function crossUlElement(number) {
+    let currentHTML = $(`.h3-${number}`).text();
+    console.log(currentHTML);
+    $(`.h3-${number}`).html(`<s>${currentHTML}</s>`);
 }
